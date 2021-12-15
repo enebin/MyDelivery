7724a55352926f6059faabb86910d4befb117052
enebin <46271447+enebin@users.noreply.github.com>
2021-12-15 15:20:50 +0900

    Initial Commit

---
+++ MyDelivery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDelivery
+//
+//  Created by 이영빈 on 2021/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyDelivery/MyDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDeliveryApp.swift
+//  MyDelivery
+//
+//  Created by 이영빈 on 2021/12/15.
+//
+
+import SwiftUI
+
+@main
+struct MyDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
